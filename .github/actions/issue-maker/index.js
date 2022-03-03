fdec34bfbccfdad9abd3a799a9eb5438a29ef705
abpedroni <abpedroni@stefanini.com>
2022-03-03 18:08:42 -0300

    octokit changes;

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -3,20 +3,20 @@ const github = require("@actions/github");
 
 async function run() {
 
-    const issueTitle = core.getInput("issue-title");
+    const issueTitle = core.getInput("issueTitle");
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
     try {
         const octokit = github.getOctokit(token);
 
-        const newIssue = await octokit.issues.create({
+        const newIssue = await octokit.rest.issues.create({
             repo: github.context.repo.repo,
             owner: github.context.repo.owner,
             title: issueTitle,
             body: jokeBody
         });
-
+        
     } catch (error) {
         core.setFailed(error.message);
     }
